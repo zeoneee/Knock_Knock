974e29fc42e2dc6855399c4eb91a01ef580f26c8
DESKTOP-FP7CP78\gram <jiwlgus048@gmail.com>
2021-12-12 15:53:49 +0900

    21/12/12 #2

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ byte colPins[COLS] = {5, 4, 3, 2};
 int check = 0;  // 블루투스랑 사전에 핸드폰에서 open 눌렀는지 안눌렀는지 체크 / check = 1이면 비밀번호 맞았을 때 문 열도록 할게요 
 int isopen = 0; // 실시간 상태 확인 / isopen = 1이면 문 열려있는 것 
 
+
 char keys[ROWS][COLS] = { 
   {'1','2','3', 'A'}, 
   {'4','5','6', 'B'}, 
@@ -69,13 +70,17 @@ void loop(){
   } 
 } 
 
+
 void Open()//성공했을시 모터를 돌려주며 성공메세지 보내기
 {
+
       myservo.write(180); 
       Serial.println("open the door"); 
-      isopen = 1;         
-      //myservo.write(0); -> 문 닫기     
-      //Serial.println("close the door");                          
+      isopen = 1;        
+
+      delay(4000); // 4초 후 문닫힘
+      Close();
+                  
 }
 
 void Close()//실패했을시 모터를 돌려주며 실패메세지 보내기
